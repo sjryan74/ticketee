0fd5658a74e257fa205acb7efe66422632369c4c
Shannon Ryan <shannon.ryan@envato.com>
2020-05-16 14:36:29 +1000

    Implement creating tickets for a project

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,37 @@
+class TicketsController < ApplicationController
+  before_action :set_project
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @ticket = @project.tickets.build
+  end
+
+  def create
+    @ticket = @project.tickets.build(ticket_params)
+
+    if @ticket.save
+      flash[:notice] = "Ticket has been created"
+      redirect_to [@project, @ticket] # looks like shorthand for redirect_to project_ticket_path(@project, @ticket)
+    else
+      flash[:notice] = "Ticket has not been created"
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def set_ticket
+    @ticket = @project.tickets.find(params[:id])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:name, :description)
+  end
+end
