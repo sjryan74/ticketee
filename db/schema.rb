c58671bb1c84e4ee9a55af8998eff0eb2f7080d1
Shannon Ryan <shannon.ryan@envato.com>
2020-05-29 11:54:18 +1000

    Added admin flag to User model, and seeded the first admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_063126) do
+ActiveRecord::Schema.define(version: 2020_05_29_014506) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_05_25_063126) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
