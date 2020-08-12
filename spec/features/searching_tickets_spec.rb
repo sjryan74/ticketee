f8883a8eafa1fe7e7cc920d808e425e4c4d20128
Shannon Ryan <shannon.ryan@envato.com>
2020-08-13 09:04:52 +1000

    Add ability to search for tickets by state or tag

--- spec/features/searching_tickets_spec.rb
+++ spec/features/searching_tickets_spec.rb
@@ -3,16 +3,19 @@ require 'rails_helper'
 RSpec.feature "Users can search for tickets matching specific criteria" do
   let(:user) { FactoryBot.create(:user) }
   let(:project) { FactoryBot.create(:project) }
+  let(:open) { State.create(name: "Open") }
+  let(:closed) { State.create(name: "Closed") }
+
   let!(:ticket_1) do
     tags = [FactoryBot.create(:tag, name: "Iteration 1")]
     FactoryBot.create(:ticket, name: "Create projects",
-      project: project, author: user, tags: tags)
+      project: project, author: user, tags: tags, state: open)
   end
 
   let!(:ticket_2) do
     tags = [FactoryBot.create(:tag, name: "Iteration 2")]
     FactoryBot.create(:ticket, name: "Create users",
-      project: project, author: user, tags: tags)
+      project: project, author: user, tags: tags, state: closed)
   end
 
   before do
@@ -27,5 +30,14 @@ RSpec.feature "Users can search for tickets matching specific criteria" do
       expect(page).to have_link "Create projects"  
       expect(page).to_not have_link "Create users"  
     end 
+  end
+
+  scenario "searching by state" do
+    fill_in "Search",	with: "state:Open" 
+    click_button "Search"
+    within(".tickets") do
+      expect(page).to have_link "Create projects"  
+      expect(page).to_not have_link "Create users"  
+    end
   end
 end
